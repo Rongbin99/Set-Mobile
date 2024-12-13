d1d1d253b654c07c59126c47c9d89ded355dbde5
Rongbin99 <rongbingu999@gmail.com>
2024-12-13 01:10:58 -0500

    Base View Basic Setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.rongbin99.set_apk"
-    compileSdk = 34
+    compileSdk = 32
 
     defaultConfig {
         applicationId = "com.rongbin99.set_apk"
         minSdk = 31
-        targetSdk = 34
+        targetSdk = 32
         versionCode = 1
         versionName = "1.0"
 
@@ -41,6 +41,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.recyclerview:recyclerview:1.3.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
